d57500c7192b4fabb934ae2344ad1ccd071334a8
nnand85 <nnand85@gmail.com>
2022-03-17 21:54:45 -0700

    Setup email functionality for signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.mailgun.org',
+  port:                 '587',
+  authentication:       :plain,
+  user_name:            ENV['MAILGUN_USERNAME'],
+  password:             ENV['MAILGUN_PASSWORD'],
+  domain:               'heroku.com',
+  enable_starttls_auto: true
+}
