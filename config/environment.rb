83b789c8a3598cd27acd73ce5118093788a25d51
nnand85 <nnand85@gmail.com>
2022-03-17 22:25:23 -0700

    Update Mailgun variables

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,13 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.mailgun.org',
   port:                 '587',
   authentication:       :plain,
-  user_name:            ENV['MAILGUN_USERNAME'],
-  password:             ENV['MAILGUN_PASSWORD'],
+  user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+  password:             ENV['MAILGUN_SMTP_PASSWORD'],
   domain:               'heroku.com',
   enable_starttls_auto: true
 }
+
+SMTP hostname: smtp.mailgun.org
+Port: 587 (recommended)
+Username: postmaster@sandboxda39b507a7224bdb98f58936b06539ad.mailgun.org
+Default password: 00e877bf50df12478a450b9e17f25cf6-dbc22c93-56836b91
